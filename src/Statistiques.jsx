3c01a961989da69d441edda85ee4cf99bdffd422
MarceauLp <marceau.lp@gmail.com>
2024-04-03 11:16:24 +0200

    Statistiques

---
+++ src/Statistiques.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Statistiques() {
+  return (
+    <div style={{ width: '50%', height: '50%', position: 'relative' }}>
+      <img src="src\assets\conf.gif" style={{ width: '50%', height: '50%', objectFit: 'cover' }} alt="Confetti" />
+    </div>
+  );
+}
