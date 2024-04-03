eed721c3aebe31178e6b3454e0e69337b462e3a2
MarceauLp <marceau.lp@gmail.com>
2024-04-03 10:36:30 +0200

    Step

--- src/Components/Core/Header.jsx
+++ src/Components/Core/Header.jsx
@@ -22,11 +22,11 @@ export default function ButtonAppBar({
             edge="start"
             color="inherit"
             aria-label="menu"
-            sx={{ mr: 2, background: CONSTANTES.BUTTON_BACKGROUND_COLOR}}
+            sx={{ mr: 2, background: CONSTANTES.BUTTON_BACKGROUND_COLOR }}
           >
             <MenuIcon />
           </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1, fontFamily: "LeoRoundedBold" }}>
             {CONSTANTES.NAME_APP}
           </Typography>
           <Button color="inherit">Statistiques</Button>
